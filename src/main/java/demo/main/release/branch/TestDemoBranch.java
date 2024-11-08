d46f1ca0668be23d2081ab8a3449d97b4929473d
ssher1001 <shersumit1001@gmail.com>
2024-11-08 18:03:45 +0530

    initial commit

---
+++ src/main/java/demo/main/release/branch/TestDemoBranch.java
@@ -0,0 +1,12 @@
+package demo.main.release.branch;
+
+public class TestDemoBranch 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("this is added by main release");
+	}
+
+}
